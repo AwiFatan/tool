d804a8c63f6ab29fcc262c3019ab104a869c7b98
Awi Fatan <59038888+AwiFatan@users.noreply.github.com>
2022-11-04 23:30:30 +0700

    Update batch-rename.js

--- batch-rename.js
+++ batch-rename.js
@@ -1,10 +1,10 @@
                     
 function rename(data1Array, data2Array) {
             var txt = '';
-            var output = '';
+            var result = '';
             for (i = 0; i < data1Array.length; i++) {
                 txt = 'ren \"' + data1Array[i] + '\" \"' + data2Array[i] + '\"\n';
-                output += txt;
+                result += txt;
             }
 
             return result;
