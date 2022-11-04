50ef9dc10d42701fd16d79162f67c56c17d01ede
Awi Fatan <59038888+AwiFatan@users.noreply.github.com>
2022-11-04 23:15:40 +0700

    Create batch-rename.js

---
+++ batch-rename.js
@@ -0,0 +1,12 @@
+                    
+function rename(data1Array, data2Array) {
+            var txt = '';
+            var output = '';
+            for (i = 0; i < data1Array.length; i++) {
+                txt = 'ren \"' + data1Array[i] + '\" \"' + data2Array[i] + '\"\n';
+                output += txt;
+            }
+
+            return result;
+}  
+
